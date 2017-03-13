e9e1beac454722e3bfeb0f7711b05a11b0f02ecb
dgrsfg <dshrwb3@163.com>
2017-03-13 13:01:08 +0800

    temp4

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
     $('#fullpage').fullpage({
     	verticalCentered: false,
-    	scrollingSpeed: 600,
+    	scrollingSpeed: 400,
     	resize: false,
     	continuousVertical: true,
     	menu:true,
-    	anchors: ['page1','page2','page3','page4','page5']
+		//navigation:true,
+		//navigationPosition:"left",
+		//navigationTooltips:['关于', '简介', '技能', '项目', '经验', '联系'],
+    	anchors: ['page1','page2','page3','page4','page5','page6'],
+        afterLoad: function(anchorLink,index){
+            switch(index){
+                case 1:
+                    break;
+                case 2:
+                    break;          
+                case 3:
+                    break;          
+                case 4:
+                    break;               
+                case 5:
+                    break;             
+                case 6:
+                    break;
+            }
+        }
     });
 });
 $("#photo2").hover(function(){
